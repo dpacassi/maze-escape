687087d7956bbdc7d78ac1762934704b4eb29b30
David Pacassi Torrico <agnostiker87@gmail.com>
2016-11-14 14:19:45 +0100

    Added links-rotate algorithm

--- js/maze-escape-algorithm.js
+++ js/maze-escape-algorithm.js
@@ -1,3 +1,4 @@
+// Laufe rechts entlang.
 while (!turtle.istAmAusgang()) {
   if (turtle.rechtsFrei()) {
     turtle.dreheRechts();
@@ -9,6 +10,22 @@ while (!turtle.istAmAusgang()) {
     // We have to go back
     turtle.dreheRechts();
     turtle.dreheRechts();
+  }
+  turtle.laufe();
+}
+
+// Laufe links entlang.
+while (!turtle.istAmAusgang()) {
+  if (turtle.linksFrei()) {
+    turtle.dreheLinks();
+  } else if (turtle.vorneFrei()) {
+    // do nothing
+  } else if (turtle.rechtsFrei()) {
+    turtle.dreheRechts();
+  } else {
+    // We have to go back
+    turtle.dreheLinks();
+    turtle.dreheLinks();
   }
   turtle.laufe();
 }
